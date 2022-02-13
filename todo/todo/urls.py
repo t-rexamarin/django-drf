08afbd4c377c6af493ba1b765b48f5a7331bf8fe
Roma <65kgm9ca@gmail.com>
2022-02-13 12:38:41 +0300

    user APIView

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from usersapp.views import UserModelViewSet
+from usersapp.views import UserModelViewSet, UserAPIView
 from project.views import ProjectModelViewSet, TodoModelViewSet
 
 
@@ -28,5 +28,6 @@ router.register('todos', TodoModelViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api/users/api-view/', UserAPIView.as_view())
 ]
--- todo/usersapp/views.py
+++ todo/usersapp/views.py
@@ -1,3 +1,6 @@
+from djangorestframework_camel_case.render import CamelCaseJSONRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from .models import User
 from .serializers import UserModelSerializer
@@ -7,3 +10,12 @@ from .serializers import UserModelSerializer
 class UserModelViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+
+
+class UserAPIView(APIView):
+    renderer_classes = [CamelCaseJSONRenderer]
+
+    def get(self, request, format=None):
+        users = User.objects.all()
+        serializer = UserModelSerializer(users, many=True)
+        return Response(serializer.data)
