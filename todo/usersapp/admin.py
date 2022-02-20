23f0357be82e8cc12a0b577c3392e67afc28ce06
Roma <65kgm9ca@gmail.com>
2022-02-20 15:29:01 +0300

    admin user creation now hash password

--- todo/usersapp/admin.py
+++ todo/usersapp/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from usersapp.models import User
+from django.contrib.auth.admin import UserAdmin
 
 
-admin.site.register(User)
+class CustomUserAdmin(UserAdmin):
+    pass
+
+
+admin.site.register(User, CustomUserAdmin)
