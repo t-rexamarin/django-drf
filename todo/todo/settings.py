581adff24387592544e1a0ca1b9598b841dadb37
Roma <65kgm9ca@gmail.com>
2022-04-04 21:28:26 +0300

    static fix

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -113,7 +113,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         # 'DIRS': [],
         # 'DIRS': [BASE_DIR / 'frontend/build'],
-        'DIRS': [BASE_DIR / 'frontend/build'],
+        'DIRS': [BASE_DIR / '../../frontend/build'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -191,8 +191,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'frontend/build/static/'
-# STATICFILES_DIRS = (BASE_DIR / '../frontend/build/static/',)
+# STATIC_ROOT = 'frontend/build/static/'
+STATICFILES_DIRS = (BASE_DIR / '../../frontend/build/static/',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
